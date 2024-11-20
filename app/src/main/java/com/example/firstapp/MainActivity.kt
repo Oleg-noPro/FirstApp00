8c91aebb9290bd2de713e6a7b5a554bf60c51b4d
Oleg <uryvaev.o@yandex.ru>
2024-11-20 15:27:18 +0300

    Input

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.firstapp
 
+import android.inputmethodservice.ExtractEditText
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextET: EditText
+    private lateinit var textTV: TextView
+    private lateinit var textLength: TextView
+    private lateinit var buttonBN: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        editTextET = findViewById(R.id.editTextET)
+        textTV = findViewById(R.id.textTV)
+        buttonBN = findViewById(R.id.buttonBN)
+        textLength = findViewById(R.id.textLength)
+
+        onButtonClick(buttonBN)
     }
+    fun onButtonClick(view: View) {
+        val sendText = editTextET.text.reversed()
+        textTV.text = sendText
+        fun lengthText(input: String) {
+            val sendLength = input.replace(" ", "").length.toString()
+            textLength.text = sendLength
+        }
+        lengthText(sendText.toString())
+    }
+
+
 }
